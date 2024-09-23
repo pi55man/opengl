c7ed277fc454860b83d3445f28a40911a6acba62
pi55man <cuinnv@gmail.com>
2024-09-23 21:48:17 +0530

    rectangel but now it works

--- src/main.cpp
+++ src/main.cpp
@@ -93,10 +93,10 @@ int main() {
 
     glDeleteShader(vertexShader);
     glDeleteShader(fragShader);
-    
+
     float vertices[] = {
-    0.5f,0.5f,0.0f,
-     0.5f,-0.5f,0.0f,
+      0.5f,0.5f,0.0f,
+      0.5f,-0.5f,0.0f,
      -0.5f,-0.5f,0.0f,
      -0.5f, 0.5f,0.0f
     };
